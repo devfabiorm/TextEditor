9b0813243381b07301e8857bdd68e6b22e492e7f
devfabiorm <fabio.evangelion.guitar@gmail.com>
2024-05-19 00:14:28 -0300

    Implemented method to opena nd read a file

--- Program.cs
+++ Program.cs
@@ -24,13 +24,29 @@
         }
     }
 
-    static void Abrir() { }
+    static void Abrir()
+    {
+        Console.Clear();
+
+        Console.WriteLine("Qual o caminho do arquivo?");
+        string path = Console.ReadLine();
+
+        using (var file = new StreamReader(path))
+        {
+            string text = file.ReadToEnd();
+            Console.WriteLine(text);
+        }
+
+        Console.WriteLine();
+        Console.ReadLine();
+        Menu();
+    }
 
     static void Editar()
     {
         Console.Clear();
 
-        Console.WriteLine("Digite seu texto abaixo");
+        Console.WriteLine("Digite seu texto abaixo (ESC para sair)");
         Console.WriteLine("-----------------------");
         string text = "";
 
